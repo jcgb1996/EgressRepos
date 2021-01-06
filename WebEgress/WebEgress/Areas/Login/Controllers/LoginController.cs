49f300016eea169f25b0b07b07e6e186f8f1ecf5
jcgb1996 <35897807+jcgb1996@users.noreply.github.com>
2021-01-05 21:44:12 -0500

    Commit Ramal main

---
+++ WebEgress/WebEgress/Areas/Login/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebEgress.Areas.Login.Controllers
+{
+    public class LoginController : Controller
+    {
+       
+
+        // POST: Login/Login/Create
+        [HttpPost]
+        public ActionResult ValidaUsuario(string Usuario)
+        {
+            try
+            {   
+                return PartialView("ContraseniaPartial");
+            }
+            catch
+            {
+                return PartialView("ContraseniaPartial");
+            }
+        }
+        
+       
+    }
+}
---
+++ WebEgress/WebEgress/Content/Scripts/LoginScrpts.js
@@ -0,0 +1,41 @@
+﻿var Login = {
+	ValidarTexto: function () {
+		var User = $("#UserLogin").val();
+		if (User != "") {
+			$("#BtnOk").removeClass("btnDisable");
+			$("#BtnOk").addClass("btnVisible");
+			$("#btnFail").addClass("btnDisable");
+			$("#btnFail").removeClass("btnVisible");
+		} else {
+			$("#BtnOk").addClass("btnDisable");
+			$("#BtnOk").removeClass("btnVisible");
+			$("#btnFail").removeClass("btnDisable");
+			$("#btnFail").addClass("btnVisible");
+		}
+	},
+
+	validarUsuario: function () {
+		var Url = '@HttpContext.Current.Request.RequestContext.RouteData.Values["Login"].ToString()/ValidaUsuario';
+		var lala = window.location.pathname;
+		var User = $("#UserLogin").val();
+		var ajaxData = JSON.stringify({ Usuario: User });
+		$.ajax({
+			cache: false,
+			dataType: 'html',
+			url: Url,
+			data: { Usuario: User },
+			type: "POST",
+			contentType: "application/json; charset=utf-8",
+			success: function (data, status, xhr) {
+
+				var result = data.d;
+
+
+			},
+			error: function (xhr, status, error) {
+				var errorr = error
+			}
+		});
+	}
+
+};
